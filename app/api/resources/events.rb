0b02d89c1377d7ad34880026b01e16c14041da9c
Will Hopper <whopper@puppetlabs.com>
2017-01-07 23:32:13 -0800

    Add PUT and DELETE routes for events

--- app/api/resources/events.rb
+++ app/api/resources/events.rb
@@ -18,6 +18,7 @@ class Events < Grape::API
     params do
       requires :user_id, type: Fixnum, desc: 'User ID.' # I think? The frontend will pass this?
       requires :title, type: String, desc: 'Title of the event.'
+      requires :category_id, type: Fixnum, desc: 'Category ID'
       optional :summary, type: String, desc: 'Short summary of the event.'
     end
     post do
@@ -33,6 +34,39 @@ class Events < Grape::API
                          :summary    => params[:summary],
                          :image_path => '')
       200
+    end
+
+    desc 'Delete an event'
+    params do
+      requires :event_id, type: Fixnum, desc: 'Event ID'
+    end
+    delete ':event_id' do
+      db = Util.connect_db
+      db[:events].where(:event_id => params[:event_id]).delete
+      '200'
+    end
+
+    # PUT /api/v1/events/:id
+    desc 'Update an event'
+    params do
+      requires :event_id, type: Fixnum, desc: 'Event ID.'
+      optional :user_id, type: Fixnum, desc: 'User ID.'
+      optional :category_id, type: Fixnum, desc: 'Event category ID'
+      optional :title, type: String, desc: 'Event title'
+      optional :summary, type: String, desc: 'Event summary'
+      optional :image_path, type: String, desc: 'Event image'
+      exactly_one_of :user_id, :category_id, :title, :summary, :image_path
+    end
+    put ':event_id' do
+      db = Util.connect_db
+      # TODO: add DB.exists helper?
+      result = db[:events].where(:event_id => params[:event_id]).count
+      unless !result.zero?
+        error! 'That event does not exist!'
+      end
+
+      key = params.keys.select {|p| p != :event_id}[0]
+      db[:events].where(:event_id => params[:event_id]).update(:"#{key}" => params[:"#{key}"])
     end
   end
 end
